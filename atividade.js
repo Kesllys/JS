e36060d1cd00d93d56de8d15eb301ca9ee96a367
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2023-03-31 22:16:20 -0300

    .

--- atividade.js
+++ atividade.js
@@ -1,11 +1,19 @@
 //listaAlunos = ['aluno1','aluno2','aluno3']
 dadosAluno = [
-    {nome: 'aluno1', email: 'email1', celular: 'celular1', materia: ['8','9','10']},
-    {nome: 'aluno2', email: 'email2', celular: 'celular2', materia: ['materia1','materia2','materia3']},
-    {nome: 'aluno3', email: 'email3', celular: 'celular3', materia: ['materia1','materia2','materia3']},
+    {nome: 'aluno1', email: 'email1', celular: 'celular1', materias:['Matemática', 'Lt', 'DS'], valorNota: ['10', '1' , '3']},
+    {nome: 'aluno2', email: 'email2', celular: 'celular2', materias: ['materia1','materia2','materia3']},
+    {nome: 'aluno3', email: 'email3', celular: 'celular3', materias: ['materia1','materia2','materia3']},
 ]
 
-for(let nome of dadosAluno){
-    console.log('nota: '[3], dadosAluno[0])
+for(let nomeAluno of dadosAluno){
+    console.log('Nome do Aluno: ', nomeAluno['nome']);
     
+    for(let aluno of dadosAluno){
+        console.log('Materia: ', aluno['materias']);
+
+        for(let nota of dadosAluno){
+            console.log(' -> Nota: ', nota['valorNota'])
+        }
+    }
+
 }
